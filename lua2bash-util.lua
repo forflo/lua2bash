6726619abef3c647d338fe532923884562f7c1b3
forflo <mayerflorian@me.com>
2017-03-22 22:00:36 +0100

    [Refactoring] Few minor whitespace changes

--- lua2bash-util.lua
+++ lua2bash-util.lua
@@ -115,11 +115,9 @@ end
 
 function tableSlice(tbl, first, last, step)
   local sliced = {}
-
   for i = first or 1, last or #tbl, step or 1 do
     sliced[#sliced+1] = tbl[i]
   end
-
   return sliced
 end
 
