125df4000b2e71e41ca06c12b37b9e5e8dc46dcf
forflo <mayerflorian@me.com>
2017-03-29 15:37:20 +0200

    [Test] for function in lua2bash-scope.lua

---
+++ tests/spec_scopeFunctions.lua
@@ -0,0 +1,77 @@
+describe(
+    "Test data structures",
+    function()
+        randomize(true)
+
+        setup("builds a symbol table with three scopes " ..
+                  "each of which containing three symbols",
+              function()
+                  config = {}
+                  config.tempVarPrefix = "TV" -- Temp Variable
+                  config.tempValPrefix = "TL" -- Temp vaLue
+                  config.environmentPrefix = "E"
+                  config.functionPrefix = "AFUN"
+                  config.tablePrefix = "TB" -- TaBle
+                  config.varPrefix = "V" -- Variable
+                  config.valPrefix = "L" -- vaLue
+                  config.indentSize = 4
+
+                  compiler = require("lua2bash-comp")
+                  scope = require("lua2bash-scope")
+                  stack = compiler.Stack()
+                  scope1 = compiler.Scope(compiler.occasions.BLOCK,
+                                          "global", 0, "global")
+                  stack:push(scope1)
+                  scope2 = compiler.Scope(compiler.occasions.FOR, "S1", 1,
+                                          scope.getPathPrefix(stack) .. "S1")
+                  stack:push(scope2)
+                  scope3 = compiler.Scope(compiler.occasions.IF, "I1", 2,
+                                          scope.getPathPrefix(stack) .. "I1")
+                  stack:push(scope3)
+                  local helperC = 0
+                  dummySymbol = compiler.Symbol(1, 1)
+                  local varNames = {
+                      {"a", "b", "c"},
+                      {"b", "c", "d"},
+                      {"d", "x", "a"}
+                  }
+                  stack:map(function(scope)
+                          helperC = helperC + 1
+                          scope:getSymbolTable():addNewSymbol(
+                              varNames[helperC][1], dummySymbol)
+                          scope:getSymbolTable():addNewSymbol(
+                              varNames[helperC][2], dummySymbol)
+                          scope:getSymbolTable():addNewSymbol(
+                              varNames[helperC][3], dummySymbol)
+                  end)
+        end)
+
+        it("tests whether whereInScope will calculate the right results",
+           function()
+               local entries = scope.whereInScope(stack, "a")
+               assert.are.same({
+                       { exists = true,
+                         symbol = dummySymbol,
+                         scope = stack:bottom() },
+                       { exists = false,
+                         symbol = nil,
+                         scope = stack:getNth(2) },
+                       { exists = true,
+                         symbol = dummySymbol,
+                         scope = stack:top() } }, entries)
+        end)
+
+        it("test whether getMostCurrentBinding really only " ..
+               "returns the most current binding",
+           function()
+               local result = scope.getMostCurrentBinding(stack, "d")
+               assert.are.same({ exists = true,
+                                 symbol = dummySymbol,
+                                 scope = stack:top() }, result)
+        end)
+
+        it("tests whether setGlobal really updates a symbol",
+           function()
+               scope.setGlobal(config, stack, "goo")
+        end)
+end)
