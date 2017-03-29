7416ae2bec6316f86f725257743db8f48b4e61b0
forflo <mayerflorian@me.com>
2017-03-29 22:37:04 +0200

    [Refactoring

--- source/lua2bash-staticPredicate.lua
+++ source/lua2bash-staticPredicate.lua
@@ -3,169 +3,172 @@ local util = require("lua2bash-util")
 -- which can be used to determine whether an AST
 -- can be statically evaluated
 
-function isStaticTbl(ast)
+local statcheck = {}
+
+function statcheck.isStaticTbl(ast)
     local result = true
     imap(ast,
          function(node)
-             result = result and isStaticExp(node)
+             result = result and statcheck.isStaticExp(node)
     end)
     return result
 end
 
-function isStaticPair(ast)
+function statcheck.isStaticPair(ast)
 end
 
-function isStaticCall(ast)
+function statcheck.isStaticCall(ast)
     local prefixExpr = ast[1]
     local arguments = util.tableSlice(ast, 2, #ast, 1)
-    return isStaticPrefix(prefixExpr) and
+    return statcheck.isStaticPrefix(prefixExpr) and
         util.ifold(
             util.imap(
                 arguments,
                 function(exp)
-                    return isStaticExp(exp) end),
+                    return statcheck.isStaticExp(exp) end),
             function(b, acc)
                 return acc and b end,
             true)
 end
 
-function isStaticFun(ast)
+function statcheck.isStaticFun(ast)
     local namelist = ast[1]
     local block = ast[2]
-    return isStaticNamelist(namelist) and
-        isStaticBlock(block)
+    return statcheck.isStaticNamelist(namelist) and
+        statcheck.isStaticBlock(block)
 end
 
-function isStaticVarlist(ast)
+function statcheck.isStaticVarlist(ast)
     return util.ifold(
         util.imap(
             ast,
             function(pExp)
-                return isStaticPrefix(pExp) end),
+                return statcheck.isStaticPrefix(pExp) end),
         function(b, acc)
             return acc and b end,
         true)
 end
 
-function isStaticExplist(ast)
-    local bools = util.imap(ast, function(exp) return isStaticExp(exp) end)
+function statcheck.isStaticExplist(ast)
+    local bools = util.imap(ast, function(exp)
+                                return statcheck.isStaticExp(exp) end)
     return util.ifold(bools, function(b, acc) return acc and b end, true)
 end
 
-function isStaticNamelist(ast)
+function statcheck.isStaticNamelist(ast)
     --TODO
     return true
 end
 
-function isStaticLocal(ast)
-    return isStaticNamelist(ast[1]) and
-        isStaticExplist(ast[2])
+function statcheck.isStaticLocal(ast)
+    return statcheck.isStaticNamelist(ast[1]) and
+        statcheck.isStaticExplist(ast[2])
 end
 
-function isStaticSet(ast)
+function statcheck.statcheck.isStaticSet(ast)
     local varlist = ast[1]
     local explist = ast[2]
-    return isStaticVarlist(varlist) and
-        isStaticExplist(explist)
+    return statcheck.isStaticVarlist(varlist) and
+        statcheck.isStaticExplist(explist)
 end
 
-function isStaticFor(ast)
+function statcheck.statcheck.isStaticFor(ast)
     if #ast == 5 then
-        return isStaticExp(ast[2])
-            and isStaticExp(ast[3])
-            and isStaticExp(ast[4])
-            and isStaticBlock(ast[5])
-            and isStaticId(ast[1])
-    else return isStaticExp(ast[2])
-            and isStaticExp(ast[3])
-            and isStaticBlock(ast[5])
-            and isStaticId(ast[1]) end
+        return statcheck.isStaticExp(ast[2])
+            and statcheck.isStaticExp(ast[3])
+            and statcheck.isStaticExp(ast[4])
+            and statcheck.isStaticBlock(ast[5])
+            and statcheck.isStaticId(ast[1])
+    else return statcheck.isStaticExp(ast[2])
+            and statcheck.isStaticExp(ast[3])
+            and statcheck.isStaticBlock(ast[5])
+            and statcheck.isStaticId(ast[1]) end
 end
 
-function isStaticForIn(ast)
+function statcheck.statcheck.isStaticForIn(ast)
     local namelist = ast[1]
     local explist = ast[2]
     local block = ast[3]
-    return isStaticNamelist(namelist) and
-        isStaticExplist(explist) and
-        isStaticBlock(block)
+    return statcheck.isStaticNamelist(namelist) and
+        statcheck.isStaticExplist(explist) and
+        statcheck.isStaticBlock(block)
 end
 
-function isStaticWhile(ast)
-    return isStaticExp(ast[1]) and isStaticBlock(ast[2])
+function statcheck.statcheck.isStaticWhile(ast)
+    return statcheck.isStaticExp(ast[1]) and statcheck.isStaticBlock(ast[2])
 end
 
-function isStaticIf(ast)
+function statcheck.statcheck.isStaticIf(ast)
     local result = true
     local elseB = #ast % 2
     for i = 1, #ast - elseB, 2 do
         result = result
-            and isStaticExpr(ast[i])
-            and isStaticBlock(ast[i + 1])
+            and statcheck.isStaticExpr(ast[i])
+            and statcheck.isStaticBlock(ast[i + 1])
     end
 
     if elseB == 1 then
-        result = result and isStaticBlock(ast[#ast])
+        result = result and statcheck.isStaticBlock(ast[#ast])
     end
     return result
 end
 
-function isStaticRepeat(ast)
-    return isStaticBlock(ast[1]) and isStaticBlock(ast[2])
+function statcheck.statcheck.isStaticRepeat(ast)
+    return statcheck.isStaticBlock(ast[1]) and statcheck.isStaticBlock(ast[2])
 end
 
-function isStaticDo(ast)
+function statcheck.statcheck.isStaticDo(ast)
     local result = true
-    imap(ast, function(node) result = result and isStaticStmt(node) end)
+    imap(ast, function(node) result = result and statcheck.isStaticStmt(node) end)
     return result
 end
 
-function isStaticRet(ast)
+function statcheck.statcheck.isStaticRet(ast)
     return util.ifold(
         util.imap(
             ast,
             function(exp)
-                return isStaticExp(exp) end),
+                return statcheck.isStaticExp(exp) end),
         function(b, acc)
             return acc and b end,
         true)
 end
 
-function isStaticStm(ast)
-    if ast.tag == "Call" then return isStaticCall(ast)
-    elseif ast.tag == "Fornum" then return isStaticFor(ast)
-    elseif ast.tag == "Local" then return isStaticLocal(ast)
-    elseif ast.tag == "Forin" then return isStaticForIn(ast)
-    elseif ast.tag == "Repeat" then return isStaticRepeat(ast)
-    elseif ast.tag == "Return" then return isStaticReturn(ast)
-    elseif ast.tag == "If" then return isStaticIf(ast)
-    elseif ast.tag == "While" then return isStaticWhile(ast)
-    elseif ast.tag == "Do" then return isStaticDo(ast)
-    elseif ast.tag == "Set" then return isStaticSet(ast)
+function statcheck.statcheck.isStaticStm(ast)
+    if ast.tag == "Call" then return statcheck.isStaticCall(ast)
+    elseif ast.tag == "Fornum" then return statcheck.isStaticFor(ast)
+    elseif ast.tag == "Local" then return statcheck.isStaticLocal(ast)
+    elseif ast.tag == "Forin" then return statcheck.isStaticForIn(ast)
+    elseif ast.tag == "Repeat" then return statcheck.isStaticRepeat(ast)
+    elseif ast.tag == "Return" then return statcheck.isStaticReturn(ast)
+    elseif ast.tag == "If" then return statcheck.isStaticIf(ast)
+    elseif ast.tag == "While" then return statcheck.isStaticWhile(ast)
+    elseif ast.tag == "Do" then return statcheck.isStaticDo(ast)
+    elseif ast.tag == "Set" then return statcheck.isStaticSet(ast)
     end
 end
 
-function isStaticBlock(ast)
+function statcheck.statcheck.isStaticBlock(ast)
     local result = true
-    imap(ast, function(node) result = result and isStaticStmt(node) end)
+    imap(ast, function(node) result = result and statcheck.isStaticStmt(node) end)
     return result
 end
 
-function isStaticExp(ast)
+function statcheck.statcheck.isStaticExp(ast)
     if ast.tag == "Op" then
-        return isStaticExp(ast[2]) and isStaticExp(ast[3])
-    elseif ast.tag == "Id" then return isStaticId(ast)
+        return statcheck.isStaticExp(ast[2]) and statcheck.isStaticExp(ast[3])
+    elseif ast.tag == "Id" then return statcheck.isStaticId(ast)
     elseif ast.tag == "True" then return true
     elseif ast.tag == "False" then return true
     elseif ast.tag == "Nil" then return true
     elseif ast.tag == "Number" then return true
     elseif ast.tag == "String" then return true
-    elseif ast.tag == "Table" then return isStaticTable(ast)
-    elseif ast.tag == "Function" then return isStaticFun(ast)
-    elseif ast.tag == "Call" then return isStaticCall(ast)
-    elseif ast.tag == "Pair" then return isStaticPair(ast)
-    elseif ast.tag == "Paren" then return isStaticExp(ast[1])
-    elseif ast.tag == "Index" then return isStaticPrefix(ast)
+    elseif ast.tag == "Table" then return statcheck.isStaticTable(ast)
+    elseif ast.tag == "Function" then return statcheck.isStaticFun(ast)
+    elseif ast.tag == "Call" then return statcheck.isStaticCall(ast)
+    elseif ast.tag == "Pair" then return statcheck.isStaticPair(ast)
+    elseif ast.tag == "Paren" then return statcheck.isStaticExp(ast[1])
+    elseif ast.tag == "Index" then return statcheck.isStaticPrefix(ast)
     else
         print("Static checker: Node type not supported!")
         print("Node type:" .. ast.tag)
@@ -173,6 +176,6 @@ function isStaticExp(ast)
     end
 end
 
-function isStaticPrefix(ast)
-    return isStaticPrefix(ast[1]) .. isStaticPrefix(ast[2])
+function statcheck.statcheck.isStaticPrefix(ast)
+    return statcheck.isStaticPrefix(ast[1]) .. statcheck.isStaticPrefix(ast[2])
 end
