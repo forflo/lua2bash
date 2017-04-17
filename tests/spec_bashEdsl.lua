859aae0c38d5926f44d09fad1f396fcf630b2833
forflo <mayerflorian@me.com>
2017-04-17 16:24:28 +0200

    [Refactoring] by review

--- tests/spec_bashEdsl.lua
+++ tests/spec_bashEdsl.lua
@@ -15,50 +15,58 @@ describe(
 
         it("tests verbatim parameters #VP",
            function()
-               assert.True(b.c("foo")() == "foo")
+               assert.True(b.string("foo")() == "foo")
         end)
 
         it("tests a simple parameter expansion use cases #PE",
            function()
-               assert.are.equal(b.pE("foo")(), [[${foo}]])
-               assert.are.equal(b.pE(b.c("foo"))(), [[${foo}]])
+               assert.are.equal(b.parenthesesE("foo")(), [[${foo}]])
+               assert.are.equal(b.parenthesesE(b.string("foo"))(), [[${foo}]])
         end)
 
         it("tests another simple parameter expansion use case #PE",
            function()
-               local str = b.pE(b.pE("foo"))()
+               local str = b.parenthesesE(b.paramExpansion("foo"))()
                assert.are.equal(str, [[\${${foo}}]])
         end)
 
         it("tests parameter expansion use cases #PE",
            function()
-               local str = b.e(b.pE("foo") .. b.pE(b.pE("bar")))()
+               local str =
+                   b.eval(
+                       b.parenthesesE("foo")
+                           .. b.paramExpansion(b.pE("bar")))()
                assert.are.equal(str, [[eval ${foo}\${${bar}}]] ) end)
 
         it("test parentheses generation #PA",
            function()
-               local str = (b.p(b.p("foo")) .. util.iterate(b.p, "bar", 3))()
+               local str =
+                   (b.parentheses(b.p("foo"))
+                        .. util.iterate(b.p, "bar", 3))()
                assert.are.equal(str, [[\((foo)\)\\\(\((bar)\)\\\)]])
-
         end)
 
         it("test different kinds of parentheses",
            function()
-               local str = b.p(b.pE("foo") .. b.pE(b.pE("bar")))()
-               local strN = b.pN(b.pE("foo") .. b.pE(b.pE("bar")))()
+               local str = b.parentheses(
+                   b.paramExpansion("foo")
+                       .. b.pE(b.pE("bar")))()
+               local strN = b.parenthesesN(
+                   b.paramExpansion("foo")
+                       .. b.pE(b.pE("bar")))()
                assert.are.same(str, [[\\\(${foo}\${${bar}}\\\)]])
                assert.are.same(strN, [[\(${foo}\${${bar}}\)]])
         end)
 
         it("test arithmetic expansion #AE",
            function()
-               local str = b.e(b.aE("1+2"))()
+               local str = b.eval(b.aE("1+2"))()
                assert.are.equal(str, [[$((1+2))]])
         end)
 
         it("test all quoting #AQ",
            function()
-               local str = b.e(b.c"plain " .. b.a("!(%:$%[])",1))()
+               local str = b.eval(b.string"plain " .. b.a("!(%:$%[])",1))()
                assert.are.equal(str, [[plain \!\(\%\:\$\%\[\]\)]])
         end)
 end)
